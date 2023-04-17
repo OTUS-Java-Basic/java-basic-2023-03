e1fdce4cb2afd13649ae0ae758932dc17c7a05fd
Mikhail Ronzhin <34248027+warlife3@users.noreply.github.com>
2023-04-17 09:59:55 +0300

    L05: lesson's code (#1)
    Co-authored-by: Mikhail Ronzhin <m.ronzhin@tinkoff.ru>

---
+++ L05-primitives/src/ru/otus/java/Primitive.java
@@ -0,0 +1,46 @@
+package ru.otus.java;
+
+/**
+ *
+ */
+public class Primitive {
+    public static void main(String[] args) {
+        int n100; // Объявить переменную
+
+
+        // пропорпор
+        System.out.println();
+
+
+        n100 = 100; // Инициализация
+
+
+
+
+
+
+        byte b = 1;
+        byte bbbb = 0b0000_1111;
+        short ns = b;
+        short q = 325;
+        short s = 123;
+        int i = 2_000_000;
+        long l = 2_000_000_000_000L;
+
+        float f = 1.0f;
+        double d = 1.0;
+
+        double dd = 0.1 + 0.2;
+
+
+        boolean flag = true; // true / false
+
+        char c1 = 'w';
+        char c2 = 'o';
+        char c3 = 'r';
+
+        String str = "Hello world".replace('e', 'a');
+        char c = '\u0042';
+        System.out.println(c);
+    }
+}
