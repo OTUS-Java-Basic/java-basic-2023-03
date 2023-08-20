f193d9f91d9d11e44cf3f546dab918cc23dc88ac
Andlek <palabugin@inbox.ru>
2023-08-20 18:34:12 +0400

    Реализовал цену прописью, но работает корректно только до 100, остальное не стал исправлять, чтобы реализовать проверку тестами

--- L15-code_parsing2/ru/otus/java/example04/task/Person.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.java.example04.task;
-
-public class Person {
-    String name; // На то, что не private не обращать внимания
-
-    public Person(String name) {
-        this.name = name;
-    }
-}
